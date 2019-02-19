78a813914a799af160f2a30cddf9849962ce0ef3
Laboratória <ma-ah02@live.com>
2019-02-19 17:02:13 -0300

    atualização html

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 document.getElementById("tweet").addEventListener("click", submit);
 
 function checkText(){
-    if(document.getElementById("message").value !== ""){
-        document.getElementById("tweet").removeAttribute("disabled");
-    }else{
-        document.getElementById("tweet").disabled = "disabled";
-    }
-    counter();
-}
-function submit(event){
-    event.preventDefault();
-    document.getElementById("result").innerHTML = document.getElementById("message").value;
-    document.getElementById("message").value = "";
+  let size = counter();
+  if(size > 0 && size <140 ){
+    document.getElementById("tweet").removeAttribute("disabled");
+  }else{
     document.getElementById("tweet").disabled = "disabled";
-    document.getElementById("counterText").innerHTML= "";
+  }
+}
+
+function submit(event){
+  event.preventDefault();
+  var lista = document.createElement("li");
+  var para = document.createElement("p");
+  var t = document.createTextNode(document.getElementById("message").value);
+  para.appendChild(t);
+  lista.appendChild(para);
+  document.getElementById("tweets").appendChild(lista);
+  document.getElementById("message").value = "";
+  document.getElementById("tweet").disabled = "disabled";
+  document.getElementById("counterText").innerHTML= "";
 }
 
 function counter(){
-    let x = document.getElementById("message").value;
-    let y = 140 - x.length;
-    document.getElementById("counterText").innerHTML= y;
-
+  let msg = document.getElementById("message").value;
+  let size = 140 - msg.length;
+  document.getElementById("counterText").innerHTML= size;
+  return size;
 }
